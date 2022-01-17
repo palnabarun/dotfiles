506598f9cea2e47a1b6d9e1d802ed58d9c1d2e33
Nabarun Pal <pal.nabarun95@gmail.com>
2022-01-17 22:57:37 +0530

    add script to install stuff on MacOS
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

---
+++ macos.sh
@@ -0,0 +1,55 @@
+#! /usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install xcode
+xcode-select --install
+
+# Install HomeBrew if not installed else update
+command -v brew
+if [[ $? != 0 ]] ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  brew update
+fi
+
+brew upgrade
+brew bundle
+
+# Install rust utilities
+cargo install starship
+cargo install exa
+cargo install bat
+cargo install ripgrep
+
+# Setup ZSH and OhMyZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# OMZ plugins
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install virtualenv and virtuelenvwrapper
+python3 -m pip install --user virtualenv virtualenvwrapper
+
+# Install Go through the graphic installer
+
+# Install VS Code extensions
+cat code/extensions.txt | xargs -L1 code --install-extension
+cp code/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+
+# Setup neovim
+# Install plugin manager
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Instll plugin dependencies
+npm update --global npm
+npm install --global yarn
+# Install and setup plugins
+nvim --headless +PlugInstall +qa
+nvim --headless +GoInstallBinaries +qa
