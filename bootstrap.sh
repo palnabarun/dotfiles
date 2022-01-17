6c241fb02798998d92dbd65e0cd72a246a4ef835
Nabarun Pal <pal.nabarun95@gmail.com>
2022-01-17 22:56:47 +0530

    add stow bootstrap script
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+if ! command -v stow &> /dev/null
+then
+    echo "GNU/Stow is not installed. Please install Stow to proceed"
+    exit 1
+fi
+
+for CONFIG in kitty nvim zsh
+do
+    mkdir -p ~/.config/$CONFIG
+    stow -v -R -t ~/.config/$CONFIG $CONFIG
+done
+
+for CONFIG in git tmux zshrc
+do
+    stow -v -R -t ~ $CONFIG
+done
+
+for CONFIG in starship
+do
+    stow -v -R -t ~/.config $CONFIG
+done
+
+stow -v -R -t ~/.ssh ssh
+stow -v -R -t ~/.gnupg gnupg
